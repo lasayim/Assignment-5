648eae78fd4cc86a8e95730ee1fe5da04ad3e324
lasayim <labib6622@gmail.com>
2025-03-02 23:10:53 +0600

    deployed

--- Js/script.js
+++ Js/script.js
@@ -4,7 +4,7 @@ const countLeft =document.getElementById('count-left').innerText
 button.addEventListener('click' , function(){
     alert('Board Updated Successfully')
     button.disabled = true; 
-    button.style.backgroundColor = 'gray'
+    
     countRight = parseInt(countRight) + 1;
     countLeft = parseInt(countLeft)  - 1;
    
@@ -15,7 +15,7 @@ btnB.addEventListener('click' , function(){
     alert('Board Updated Successfully')
     btnB.disabled = true;
     
-    btnB.style.backgroundColor = 'gray'
+   
   
 })
 // btn-3
@@ -24,7 +24,7 @@ btnC.addEventListener('click' , function(){
     alert('Board Updated Successfully')
     btnC.disabled = true;
     
-    btnC.style.backgroundColor = 'gray'
+    
 })
 // btn-4
 const btnD = document.getElementById('btn-4');
@@ -32,7 +32,7 @@ btnD.addEventListener('click' , function(){
     alert('Board Updated Successfully')
     btnD.disabled = true;
     
-    btnD.style.backgroundColor = 'gray'
+    
 })
 // btn-5
 const btnE = document.getElementById('btn-5');
@@ -40,7 +40,7 @@ btnE.addEventListener('click' , function(){
     alert('Board Updated Successfully')
     btnE.disabled = true;
     
-    btnE.style.backgroundColor = 'gray'
+   
 })
 // btn-6
 const btnF = document.getElementById('btn-6');
@@ -48,7 +48,7 @@ btnF.addEventListener('click' , function(){
     alert('Board Updated Successfully')
     btnF.disabled = true;
     
-    btnF.style.backgroundColor = 'gray'
+    
 })
 
 // if(button.disabled && btnB.disabled && btnC.disabled && btnD.disabled && btnE.disabled && btnF.disabled){
