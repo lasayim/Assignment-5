390006c1806dde8cd1d609192fcddc5a842846ae
lasayim <labib6622@gmail.com>
2025-03-02 23:01:32 +0600

    deployed

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,8 @@ const button = document.getElementById('btn-1');
 const countRight =document.getElementById('count-right').innerText
 const countLeft =document.getElementById('count-left').innerText
 button.addEventListener('click' , function(){
-    button.disabled = true;
-    
+    alert('Board Updated Successfully')
+    button.disabled = true; 
     button.style.backgroundColor = 'gray'
     countRight = parseInt(countRight) + 1;
     countLeft = parseInt(countLeft)  - 1;
@@ -12,6 +12,7 @@ button.addEventListener('click' , function(){
 // btn-2
 const btnB = document.getElementById('btn-2');
 btnB.addEventListener('click' , function(){
+    alert('Board Updated Successfully')
     btnB.disabled = true;
     
     btnB.style.backgroundColor = 'gray'
@@ -20,6 +21,7 @@ btnB.addEventListener('click' , function(){
 // btn-3
 const btnC = document.getElementById('btn-3');
 btnC.addEventListener('click' , function(){
+    alert('Board Updated Successfully')
     btnC.disabled = true;
     
     btnC.style.backgroundColor = 'gray'
@@ -27,6 +29,7 @@ btnC.addEventListener('click' , function(){
 // btn-4
 const btnD = document.getElementById('btn-4');
 btnD.addEventListener('click' , function(){
+    alert('Board Updated Successfully')
     btnD.disabled = true;
     
     btnD.style.backgroundColor = 'gray'
@@ -34,6 +37,7 @@ btnD.addEventListener('click' , function(){
 // btn-5
 const btnE = document.getElementById('btn-5');
 btnE.addEventListener('click' , function(){
+    alert('Board Updated Successfully')
     btnE.disabled = true;
     
     btnE.style.backgroundColor = 'gray'
@@ -41,8 +45,15 @@ btnE.addEventListener('click' , function(){
 // btn-6
 const btnF = document.getElementById('btn-6');
 btnF.addEventListener('click' , function(){
+    alert('Board Updated Successfully')
     btnF.disabled = true;
     
     btnF.style.backgroundColor = 'gray'
 })
+
+// if(button.disabled && btnB.disabled && btnC.disabled && btnD.disabled && btnE.disabled && btnF.disabled){
+//     alert('all button are clicked')
+// }
+
+
 
